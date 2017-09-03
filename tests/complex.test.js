b1522bf6435b8eed9981241095ea6d70a036c83c
Harry Sarson <harry.sarson@hotmail.co.uk>
2017-09-03 13:51:00 +0100

    Fixes syntax error

--- tests/complex.test.js
+++ tests/complex.test.js
@@ -684,7 +684,7 @@ describe("Complex", function () {
 
       if (tests[i].fn) {
 
-        it(describeTest(test), function () {
+        it(describeTest(tests[i]), function () {
           try {
             assert.equal(tests[i].expect, new Complex(tests[i].set)[tests[i].fn](tests[i].param).toString());
           } catch (e) {
@@ -693,7 +693,7 @@ describe("Complex", function () {
         });
 
       } else {
-        it(describeTest(test), function () {
+        it(describeTest(tests[i]), function () {
           try {
             assert.equal(tests[i].expect, new Complex(tests[i].set).toString());
           } catch (e) {
